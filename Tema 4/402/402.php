cf2c4e278f4393175d3e05c74cab99b76d7e1b06
Josep <98087218+Josep02@users.noreply.github.com>
2022-10-18 22:48:23 +0200

    402

---
+++ Tema 4/402/402.php
@@ -0,0 +1,38 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$url = $_POST["url"];
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> 402 </title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>Nom:</th>
+        <td><?= $name ?></td>
+    </tr>
+    <tr>
+        <th>Email:</th>
+        <td><?= $email ?></td>
+    </tr>
+    <tr>
+        <th>Telefon:</th>
+        <td><?= $phone ?></td>
+    </tr>
+    <tr>
+        <th>URL:</th>
+        <td><?= $url ?></td>
+    </tr>
+</table>
+
+</body>
+</html>
