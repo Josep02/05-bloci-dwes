b275fa8c7366980debff01614307a6c2711a0c9b
Josep <98087218+Josep02@users.noreply.github.com>
2022-11-08 13:45:51 +0100

    502

--- Tema 5/502.php
+++ Tema 5/502.php
@@ -7,9 +7,9 @@ try {
                                                 (:id, :title, :overview, :release_date, :movie_status, :tagline, :vote_average, :vote_count, :poster, :genre_id)');
     $a->execute([
         ':id' => '476890',
-        ':title' => 'Avatar',
+        ':title' => 'Dune',
         ':overview' => '',
-        ':release_date' => '2011-02-23',
+        ':release_date' => '2021-02-23',
         ':movie_status' => 'Released',
         ':tagline' => '',
         ':vote_average' => '0',
@@ -17,8 +17,23 @@ try {
         ':poster' => '',
         ':genre_id' => '18'
     ]);
-
     echo $a->rowCount();
+
+    $b = $pdo->prepare('UPDATE movie SET title= :copia WHERE title= :title ');
+    $b->execute([
+        ':title' => 'Dune',
+        ':copia' => 'Dune_copia'
+    ]);
+    echo $b->rowCount();
+
+    $c = $pdo->prepare('INSERT INTO genre (id, name, number_of_movies) VALUES (:id, :name, :number_of_movies)');
+    $c->execute([
+       ':id' => 10,
+       ':name' => 'Sci-Fi',
+       ':number_of_movies' => ''
+    ]);
+    echo $c->rowCount();
+
 
 } catch (PDOException $e){
     echo 'Error: ' . $e->getMessage();
